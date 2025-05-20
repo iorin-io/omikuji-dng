d6f4f0a65c09808737c64bac8129b19498e0adba
iorin-io <iodere77@gmail.com>
2025-05-20 14:52:20 +0900

    d.tsを追加

---
+++ src/types/encoding-japanese.d.ts
@@ -0,0 +1,6 @@
+// src/types/encoding-japanese.d.ts
+declare module "encoding-japanese" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const EncodingJapanese: any;
+  export = EncodingJapanese;
+}
